78fb12065bfccdaa2c80a64ab8db660784a26e3e
XtremeAAA <achintsingh777@gmail.com>
2025-05-03 15:04:28 +1000

    js script updates mainly

--- script.js
+++ script.js
@@ -42,3 +42,21 @@ const observer = new IntersectionObserver(
 
 scrollElements.forEach((el) => observer.observe(el));
 });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+};
